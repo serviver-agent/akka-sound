3e7ede058c11ebbba97a663736e09834d968182f
KazuyaMiayshita <miy1924@gmail.com>
2022-07-14 08:06:40 +0900

    GUIの構築・実行をイベント・ディスパッチ・スレッド上で行う
    cf. https://docs.oracle.com/javase/jp/8/docs/api/javax/swing/package-summary.html#threading

--- src/main/scala/com/serviveragent/Main.scala
+++ src/main/scala/com/serviveragent/Main.scala
@@ -7,7 +7,7 @@ import com.serviveragent.guitest.GUIMain
 object Main extends App {
   val audioMain = new AudioMain()
   new Thread(audioMain).start()
-  val guiMain = new GUIMain(() => println("close button pressed"))
+  GUIMain.start(() => println("close button pressed"))
   println("press enter to stop")
   io.StdIn.readLine()
   println("stop")
--- src/main/scala/com/serviveragent/guitest/GUIMain.scala
+++ src/main/scala/com/serviveragent/guitest/GUIMain.scala
@@ -3,14 +3,24 @@ package com.serviveragent.guitest
 import java.awt.event.{WindowAdapter, WindowEvent}
 import javax.swing.*
 
-class GUIMain(onClose: () => Unit) {
+class GUIMain private (onClose: () => Unit) extends Runnable {
 
-  val f = new JFrame("Audio Spectrum")
-  f.setSize(1024, 768)
-  f.setLocation(200, 200)
-  f.addWindowListener(new WindowAdapter() {
-    override def windowClosing(e: WindowEvent): Unit = onClose()
-  })
-  f.setVisible(true)
+  override def run(): Unit = {
+    val f = new JFrame("Audio Spectrum")
+    f.setSize(1024, 768)
+    f.setLocation(200, 200)
+    f.addWindowListener(new WindowAdapter() {
+      override def windowClosing(e: WindowEvent): Unit = onClose()
+    })
+    f.setVisible(true)
+  }
+
+}
+
+object GUIMain {
+
+  def start(onClose: () => Unit): Unit = {
+    SwingUtilities.invokeLater(new GUIMain(onClose))
+  }
 
 }
