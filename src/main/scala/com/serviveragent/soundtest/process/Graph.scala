b7b55964d8ec2d696b793aa3db1c8364a8ee3b1b
KazuyaMiayshita <miy1924@gmail.com>
2022-07-31 10:20:37 +0900

    微リファ

--- src/main/scala/com/serviveragent/soundtest/process/Graph.scala
+++ src/main/scala/com/serviveragent/soundtest/process/Graph.scala
@@ -17,11 +17,11 @@ class Graph {
   val triGen = triangleGenerator("triangle-gen", 440.0)
   val lineGen = lineGenerator("line-gen", 0.5)
 
-  val mulFn: (Sample, Sample) => Sample = _ * _
-  val addFn: (Sample, Sample) => Sample = _ * _
+  val mulFn: Seq[Sample] => Sample = _.product
+  val addFn: Seq[Sample] => Sample = _.sum
 
-  val triNode = new Node.GeneratorNode(triGen)
-  val lineNode = new Node.GeneratorNode(lineGen)
+  val triNode = new Node.ProcessorNode(triGen)
+  val lineNode = new Node.ProcessorNode(lineGen)
   val mulNode1 = new Node.SimpleFunctionNode(mulFn)
   val mulNode2 = new Node.SimpleFunctionNode(mulFn)
   val addNode = new Node.SimpleFunctionNode(addFn)
@@ -41,7 +41,14 @@ class Graph {
 
   val valueMemo: mutable.Map[Node, Option[Sample]] = mutable.HashMap(nodes.map(n => (n, None)): _*)
   val states: mutable.Map[Node, Any] =
-    mutable.HashMap(nodes.collect { case n: Node.GeneratorNode => n }.map(n => (n, n.processor.initialState)): _*)
+    mutable.HashMap(nodes.collect { case n: Node.ProcessorNode => n }.map(n => (n, n.processor.initialState)): _*)
+
+  private def getState(node: Node.ProcessorNode): node.processor.S = {
+    states(node).asInstanceOf[node.processor.S]
+  }
+  private def setState(node: Node.ProcessorNode, state: node.processor.S): Unit = {
+    states(node) = state
+  }
 
   def process(env: Environment, t: Long, in: Sample): Sample = {
     def run(node: Node): Sample = {
@@ -52,11 +59,11 @@ class Graph {
           case Some(value) => value
           case None =>
             node match {
-              case node: Node.GeneratorNode =>
-                val (o, s) = node.processor.process(env, t, (), states(node).asInstanceOf[node.processor.S])
-                states(node) = s.asInstanceOf[node.processor.S]
+              case node: Node.ProcessorNode =>
+                val (o, s) = node.processor.process(env, t, (), getState(node))
+                setState(node, s)
                 o
-              case node: Node.SimpleFunctionNode => node.fn(sourceValues(0), sourceValues(1)) // うーん
+              case node: Node.SimpleFunctionNode => node.fn(sourceValues)
               case Node.Source                   => in
               case Node.Dest                     => sourceValues.sum
             }
@@ -67,8 +74,6 @@ class Graph {
 
     val v = run(Node.Dest)
 
-    println(valueMemo)
-
     valueMemo.keys.foreach(k => valueMemo(k) = None)
     v
   }
@@ -78,8 +83,8 @@ object Graph {
 
   sealed trait Node
   object Node {
-    class GeneratorNode(val processor: Processor[Unit, Sample, _]) extends Node
-    class SimpleFunctionNode(val fn: (Sample, Sample) => Sample) extends Node
+    class ProcessorNode(val processor: Processor[Unit, Sample, _]) extends Node
+    class SimpleFunctionNode(val fn: Seq[Sample] => Sample) extends Node
     case object Source extends Node
     case object Dest extends Node
   }
